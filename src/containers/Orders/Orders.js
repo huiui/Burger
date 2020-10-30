851d33721bc0616de8caee45fc91c663741e03c0
Zihui Wang <zihui0714@gmail.com>
2020-10-29 18:17:15 -0700

    finish data fetching

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
+import {connect} from "react-redux";
 import Order from "../../components/Order/Order";
 import axios from "../../axios-orders";
 import withErrorHandler from "../withErrorHandler/withErrorHandler";
+import * as actions from "../../store/actions/index";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
-const Orders = () => {
-  const [state, setState] = useState({ orders: [], loading: true });
-  useEffect(() => {
-    axios
-      .get("/orders.json")
-      .then((res) => {
-        const fetchedOrders = [];
-        for (let key in res.data) {
-          fetchedOrders.push({ ...res.data[key], id: key });
-        }
-        setState({ loading: false, orders: fetchedOrders });
-      })
-      .catch((err) => {
-        setState({ loading: false });
-      });
-  }, []);
+const Orders = (props) => {
+  useEffect(props.onFetchOrders, []);
+  let orders = <Spinner/>;
+  if (!props.loading){
+    orders = props.currentOrders.map((order) => (
+      <Order
+        key={order.id}
+        ingredients={order.ingredients}
+        price={order.price}
+      />
+    ))
+  }
   return (
     <div>
-      {state.orders.map((order) => (
-        <Order
-          key={order.id}
-          ingredients={order.ingredients}
-          price={order.price}
-        />
-      ))}
+      {orders}
     </div>
   );
 };
 
-export default withErrorHandler(Orders, axios);
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchOrders: ()=> dispatch(actions.fetchOrders())
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    currentOrders: state.order.orders,
+    loading: state.order.loading,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
